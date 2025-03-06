19766a5d37525f4defa4d8b128b5676cdb7777b3
Mikolaj231 <mikolajmelnyk16@gmail.com>
2025-03-06 14:03:54 +0100

    Update - style & body

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,60 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  //const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='App'>
+      <header className="App-header">
+        <div className='logo'></div>
+        <h1>Character Counter</h1>
+      </header>
+
+      {/*main*/}
+      <main className='App-main'>
+
+        <section className='App-section'>
+
+          <div className='Title-area'>
+            <h1>Analyze your text in real-time.</h1>
+          </div>
+          {/*input text area*/}
+          <div className='input-area'>
+            <textarea
+              /*value={text}*/
+              className='text-input'
+              /*onChange={handleTextChange}*/
+              placeholder='Start typing here... (or paste your text)'
+            ></textarea>
+          </div>
+          <div className='Approx_reading-time'>
+            <h3>Approx. Reading time:</h3>
+          </div>
+          {/*output text area*/}
+          <div className='output-area'>
+            <div className='total-chr'>
+              <p>00</p>
+              <h2>Total Characters</h2>
+            </div>
+            <div className='word-count'>
+              <p>00</p>
+              <h2>Word Count</h2>
+            </div>
+            <div className='sentence-count'>
+              <p>00</p>
+              <h2>Sentence Count</h2> 
+            </div>
+          </div>
+
+          <div className='letter-density'>
+            <h2> Letter Density: </h2>
+          </div>
+
+        </section>
+
+      </main>
+    </div>
   )
 }
 
